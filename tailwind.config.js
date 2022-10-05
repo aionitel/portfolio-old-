cf658bade7cfce708e2fd316d5c9dbd73250a79c
Alex Ionitel <oranggsoda@gmail.com>
2022-10-05 17:39:51 -0400

    using custom cream and lead colours as background colours for light and dark themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,42 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkTheme: 'class',
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        ubuntu: ["Ubuntu"],
+        karla: ["Karla"],
+        quando: ["Quando"],
+        source: ["Source Sans Pro"],
+        noto: ["Noto Sans"],
+        inter: ["Inter"]
+      },
+      colors: {
+        'white': '#fff',
+        'red': '#ff0000',
+        'cyan': '#00ffff',
+        'blue': '#1552F0',
+        'cream': '#F0E7DB',
+        'lead': '#201f24',
+        'gray': '#171717',
+        'lightgray': '#202020',
+        "medGray": "#9c9c9c",
+        "chartGray": "#666",
+        'almostBlack': "#141414",
+        "pink": '#FFCCCC',
+        "black": '#000000'
+      }
+    },
   },
-  plugins: [],
+  purge: {
+    safelist: [
+      'text-green-400',
+      'text-red',
+      'text-base',
+      'text-center',
+    ]
+  }
 }
