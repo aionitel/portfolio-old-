66ad4300930e8ec7b3fbdce9f6582de1c875bacb
Alex Ionitel <oranggsoda@gmail.com>
2022-10-07 19:59:06 -0400

    better mobile header sizes, header has a backdrop blue

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -6,10 +6,13 @@ const Header: React.FC = () => {
   const [hover, setHover] = useState<boolean>(false);
 
   return (
-    <div className='flex justify-between w-screen'>
-      <div className='flex hover:cursor-pointer text-2xl m-1' onMouseOver={() => setHover(true)} onMouseOut={() => setHover(false)}>
+    <div className='flex justify-between w-screen sticky top-0 z-[1000] backdrop-blur-sm'>
+      <div className='flex hover:cursor-pointer m-1 transition-all text-4xl lg:text-3xl' onMouseOver={() => setHover(true)} onMouseOut={() => setHover(false)} onClick={() => {
+          document.body.scrollTop = 0; // For Safari
+          document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+      }}>
         <BiAtom style={{ transform: hover ? 'rotate(270deg)' : '' }} className='transition-all duration-500 m-1.5' />
-        <h1 className='mt-0.5 text-2xl font-caveat'>Alex Ionitel</h1>
+        <h1 className='mt-0.5 font-caveat'>Alex Ionitel</h1>
       </div>
       <ThemeSwitch />
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,26 @@
 import type { NextPage } from 'next'
+import TypewriterComponent from 'typewriter-effect';
 
 const Home: NextPage = () => {
   return (
     <div>
-      
+      <div
+        className='h-screen'
+        style={{
+          fontSize: 0
+        }}
+      >
+        <TypewriterComponent 
+          options={{
+            'strings': ['Welcome to my homepage.'],
+            autoStart: true,
+            loop: true,
+          }}
+        />
+      </div>
+      <div className='h-screen bg-white dark:bg-white'>
+        hello
+      </div>
     </div>
   )
 }
