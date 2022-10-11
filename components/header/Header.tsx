8dbbb0b0f90dbcc1952a22d950652fa5eef85faf
Alex Ionitel <oranggsoda@gmail.com>
2022-10-11 17:12:09 -0400

    better Header margin on desktop

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -7,13 +7,15 @@ const Header: React.FC = () => {
 
   return (
     <div className='flex justify-between w-screen sticky top-0 z-[1000] backdrop-blur-sm'>
-      <div className='flex hover:cursor-pointer m-3 lg:m-1 transition-all text-4xl lg:text-3xl' onMouseOver={() => setHover(true)} onMouseOut={() => setHover(false)} onClick={() => {
+      <div className='flex hover:cursor-pointer m-3 lg:m-3 lg:mt-2 transition-all text-4xl lg:text-3xl' onMouseOver={() => setHover(true)} onMouseOut={() => setHover(false)} onClick={() => {
         window.scrollTo({top: 0, behavior: 'smooth'}); // smoothly scrolling to top of page
       }}>
         <GiOrangeSlice style={{ transform: hover ? 'rotate(360deg)' : '' }} className='text-orange-500 transition-all duration-500 m-1.5' />
         <h1 className='mt-0.5 font-caveat'>Alex Ionitel</h1>
       </div>
-      <ThemeSwitch />
+      <div className='lg:mt-1.5 lg:mr-2'>
+        <ThemeSwitch />
+      </div>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head';
 import TypewriterComponent from 'typewriter-effect';
+import { BiDownvote } from 'react-icons/bi'
 
 const Home: NextPage = () => {
   return (
@@ -9,17 +10,24 @@ const Home: NextPage = () => {
         <title>Alex Ionitel</title>
       </Head>
       <div
-        className='text-center lg:text-left text-7xl lg:text-9xl my-8 lg:mx-72'
+        className='text-center lg:text-left text-7xl lg:text-9xl my-8 lg:mx-72 h-[600px] lg:h-[700px]'
       >
         <TypewriterComponent 
           options={{
-            'strings': ['Welcome to my site.', 'I am a full stack developer working out of Canada.'],
+            strings: ['Hello'],
             autoStart: true,
-            loop: true,
-            delay: 70,
-            deleteSpeed: 50
+            deleteSpeed: 50,
+            delay: 100
           }}
         />
+      </div>
+      <div className='flex justify-center text-7xl animate-pulse hover:animate-none hover:cursor-pointer' onClick={() => {
+        window.scrollTo({ top: 700, behavior: 'smooth' })
+      }}>
+        <BiDownvote />
+      </div>
+      <div className=''>
+
       </div>
     </div>
   )
