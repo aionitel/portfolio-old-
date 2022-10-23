838f7b24d64a9eeb7d2702a91b3cfd427882fb81
Alex Ionitel <oranggsoda@gmail.com>
2022-10-23 00:50:43 -0400

    have all icons on Footer, name, date, and copyright symbol

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -3,12 +3,18 @@ import { BsFileRichtext, BsGithub, BsLinkedin } from 'react-icons/bs'
 
 const Footer: React.FC = () => {
   return (
-    <div className='border-t border-silverPink border-dotted h-32 flex justify-between'>
-      <h1>© 2022 Alex Ionitel</h1>
-      <div className='flex'>
-        <BsFileRichtext />
-        <BsGithub />
-        <BsLinkedin />
+    <div className='border-t border-silverPink border-dotted h-32 flex-row text-center lg:flex lg:justify-between my-10 px-8'>
+      <h1 className='mt-16 text-sm'>© 2022 Alex Ionitel</h1>
+      <div className='flex text-4xl lg:mt-16 mt-8'>
+        <div className='h-2 hover:cursor-pointer ml-auto'>
+          <BsFileRichtext />
+        </div>
+        <div className='mx-2'>
+          <BsGithub />
+        </div>
+        <div className='mr-auto'>
+          <BsLinkedin />
+        </div>
       </div>
     </div>
   )
