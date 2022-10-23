55674437ce6309fc41a5b9ae8b9cf332f8837368
Alex Ionitel <oranggsoda@gmail.com>
2022-10-23 19:40:17 -0400

    better Footer, with better icon transitions

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -6,13 +6,13 @@ const Footer: React.FC = () => {
     <div className='border-t border-silverPink border-dotted h-32 flex-row text-center lg:flex lg:justify-between my-10 px-8'>
       <h1 className='mt-16 text-sm'>© 2022 Alex Ionitel</h1>
       <div className='flex text-4xl lg:mt-16 mt-8'>
-        <a className='h-2 hover:cursor-pointer ml-auto hover:scale-110'>
+        <a className='h-2 hover:cursor-pointer ml-auto hover:scale-110 transition-all duration-300'>
           <BsFileRichtext />
         </a>
-        <a className='mx-3' href='https://github.com/oranges0da' target='_blank' rel="noreferrer">
+        <a className='mx-3 hover:scale-110 transition-all duration-300' href='https://github.com/oranges0da' target='_blank' rel="noreferrer">
           <BsGithub />
         </a>
-        <a className='mr-auto' href='https://www.linkedin.com/in/alex-ionitel-27b625254' target='_blank' rel="noreferrer">
+        <a className='hover:scale-110 transition-all duration-300 mr-auto' href='https://www.linkedin.com/in/alex-ionitel-27b625254' target='_blank' rel="noreferrer">
           <BsLinkedin />
         </a>
       </div>
