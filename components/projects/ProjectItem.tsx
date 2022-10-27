ecae9ecfe57a172266e0bb9a5328c01bacfde4a1
Alex Ionitel <oranggsoda@gmail.com>
2022-10-26 20:52:48 -0400

    better ProjectItem link buttons, looking good on mobile as well

--- components/projects/ProjectItem.tsx
+++ components/projects/ProjectItem.tsx
@@ -16,14 +16,14 @@ const ProjectItem: React.FC<ProjectItemProps> = ({ project }) => {
           <h1>{project.title}</h1>
         </div>
         <p className='text-chartGray ml-1'>{project.desc}</p>
-        <div className='flex'>
-          <a className='hover:cursor-pointer mx-2 bg-orange-400 rounded-md hover:bg-orange-500 flex' data-tip={<div></div>}>
-            <h1>Visit Site</h1>
-            <BiLinkExternal href={project.website} target='_blank'  />
+        <div className='flex mt-7 justify-center lg:justify-end'>
+          <a className='hover:cursor-pointer mx-2 bg-orange-400 rounded-md hover:rounded-none transition-all flex' data-tip={<div></div>}>
+            <h1 className='py-2 px-2.5'>Visit Site</h1>
+            <BiLinkExternal className='text-3xl mt-[5px] pr-1.5' />
           </a>
-          <a href={project.code} target='_blank' rel="noreferrer" className='rounded-md bg-orange-400 hover:bg-orange-500 flex' data-tip={<div></div>}>
-            <h1>View Code</h1>
-            <BiCodeAlt />
+          <a href={project.code} target='_blank' rel="noreferrer" className='rounded-md bg-orange-400 hover:rounded-none transition-all flex' data-tip={<div></div>}>
+            <h1 className='p-2'>View Code</h1>
+            <BiCodeAlt className='text-3xl mt-[5px] pr-1.5' />
           </a>
         </div>
       </div>
