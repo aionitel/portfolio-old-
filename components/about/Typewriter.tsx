8b3f5380c05e15a2da2afe00c23e025aba7f4c44
Alex Ionitel <oranggsoda@gmail.com>
2023-03-01 17:18:36 -0500

    ignore eslint rules to deploy

--- components/about/Typewriter.tsx
+++ components/about/Typewriter.tsx
@@ -5,7 +5,7 @@ import TypewriterComponent from 'typewriter-effect';
 const Typewriter: React.FC = () => {
   return (
     <span className='2xl:text-9xl md:text-8xl text-6xl mx-20'>
-      <h1 className='inline mr-2'>Hello there! I'm a full stack software engineer based out of Canada 🇨🇦 who loves oranges 🍊 and I love working with</h1>
+      <h1 className="inline m-2">Hello there! I'm a full stack software engineer based out of Canada 🇨🇦 who loves oranges 🍊 and I love working with</h1>
       <TypewriterComponent
         options={{
           strings: [
--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -4,12 +4,12 @@ import { BsGithub, BsLinkedin } from 'react-icons/bs'
 const Footer: React.FC = () => {
   return (
     <div className='border-t border-silverPink border-dotted h-32 flex-row text-center lg:flex lg:justify-between my-10 px-8'>
-      <h1 className='mt-16 text-sm text-chartGray'>© 2022 Alex Ionitel</h1>
+      <h1 className='mt-16 text-sm text-chartGray'>© 2023 Alex Ionitel</h1>
       <div className='flex text-4xl lg:mt-16 my-10 pb-20'>
         <a className='mx-3 hover:scale-110 transition-all duration-300' href='https://github.com/oranges0da' target='_blank' rel="noreferrer">
           <BsGithub />
         </a>
-        <a className='hover:scale-110 transition-all duration-300 mr-auto' href='https://www.linkedin.com/in/alex-ionitel-726393255' target='_blank' rel="noreferrer">
+        <a className='hover:scale-110 transition-all duration-300 mr-auto' href='https://linkedin.com/in/alex-ionitel' target='_blank' rel="noreferrer">
           <BsLinkedin />
         </a>
       </div>
